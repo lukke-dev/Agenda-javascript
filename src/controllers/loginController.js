2dc3b0722c81ba7437351f5c41f6381acd8ac79b
Lucas <lucas.ffr2014@gmail.com>
2021-06-14 02:12:08 -0300

    teste commit

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,8 +4,20 @@ exports.index = (req, res) => {
     res.render('login');
 };
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
+try{
     const login = new Login(req.body);
-    login.register();
-    res.send(login.errors);
+    await login.register();
+    if(login.errors.length > 0) {
+    req.flash('errors', login.errors);
+    req.session.save(function() {
+        return res.redirect('back');
+    });
+    return;
+    }
+    return res.send(login.errors);
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }    
 };
